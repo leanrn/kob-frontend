c5dbbbfcbfc2dff22b4b594c41d9559959f29592
yuanqian <2910865195@qq.com>
2023-03-25 17:39:49 +0800

    init

---
+++ src/components/DemoItem.vue
@@ -0,0 +1,37 @@
+<template>
+    <button v-on:click="increase">点我数字增加</button>
+    <h1 class="color">{{number}}</h1>
+</template>
+
+<!-- <script setup>
+import { ref } from 'vue';
+
+let number=ref(0);
+const increase = ()=>{
+    number.value++;
+};
+</script> -->
+
+<script>
+import { ref } from 'vue';
+
+export default{
+    setup(){
+        let number= ref(0);
+        const increase = ()=>{
+            number.value++;
+        }
+        return{
+            number,
+            increase
+        }
+    }
+  
+}
+</script>
+
+<style scoped>
+.color{
+    color: red;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import DemoItem from '../components/DemoItem.vue';
+
+
+</script>
+
+<template>
+  <DemoItem/>
+</template>
